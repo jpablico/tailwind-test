44459e3f2295514993bcb6664e5c0226375fb290
Yashido <jbpablico27@gmail.com>
2023-07-19 13:48:47 -0600

    Tailwind config++ | i-o css files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./home/yashido/projects/tailwind-test.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
